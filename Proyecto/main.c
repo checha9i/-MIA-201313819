9a77c7ec9b7100b074912df1efa872c949f08851
checha9i <checha.db63@gmail.com>
2016-08-03 20:45:05 -0600

    creacion mbr

--- Proyecto/main.c
+++ Proyecto/main.c
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <time.h>
 void compiler();
 FILE *archivo;
 char comando[200];
-
+/* Definicion de Structs*/
 typedef struct mkdiskc{
     char auxnombre[25];
     char auxpath[100];
@@ -27,17 +28,6 @@ typedef struct fidskc{
     char auxnombre[25];
     char anadir[25];
 }fdiskc;
-
-typedef struct Mbrdisk{
-  /*  int mbr_tamaño;
-    char mbr_fecha_creacion[17];
-    int mbr_disk_signature;
-    partition particion1;
-    partition particion2;
-    partition particion3;
-    partition particion4;*/
-}Mbrdisk;
-
 typedef struct partition{
     char part_status[1];
     char part_type[1];
@@ -47,37 +37,70 @@ typedef struct partition{
     char part_name[16];
 }partition;
 
+typedef struct Mbrdisk{
+    int mbr_tamano;
+    char mbr_fecha_creacion[17];
+    int mbr_disk_signature;
+    partition *particion;
+    partition *particion2;
+    partition *particion3;
+    partition *particion4;
+}Mbrdisk;
+/*termina definicion de structs*/
+
+/* main*/
 int main()
 {
+    printf("*******************Fase1******************************\n");
     compiler();
     return 0;
 }
-
+/*funcion unica*/
 void compiler(){
     int a=0;
-
+    mkdiskc *  nuevo=malloc(sizeof(mkdiskc));
+    rmdiskc *  removernodo=malloc(sizeof(rmdiskc));
+    Mbrdisk * mbr=malloc(sizeof(Mbrdisk));
+    fdiskc * disk=malloc(sizeof(fdiskc));
+    struct tm *info;
+    char unidad;
+    /*banderas comandos*/
     bool mkdisk=false;
     bool rmdisk=false;
     bool fdisk=false;
-mkdiskc *  nuevo=malloc(sizeof(mkdiskc));
-rmdiskc *  removernodo=malloc(sizeof(rmdiskc));
-
-    char unidad;
+    /*banderas parametros*/
     bool size=false;
     bool pat = false;
     bool name = false;
     bool unitb=false;
+    bool type=false;
+    bool fit=false;
+    bool deletee=false;
+    bool addd=false;
+    /* loop del programa*/
     while(a==0){
         fgets(comando,200,stdin);
-
-
         char* token;
         token  = strtok(comando," ");
+        /*ya teniendo el token lo recorremos*/
         while(token!=NULL){
-
             if(strcasecmp(token,"mkdisk")==0){
-                printf("Comando: mkdisk\n");
+                printf("***************************************\n");
+                printf("        -Comando: mkdisk-\n");
+                printf("***************************************\n");
                 mkdisk=true;
+            }
+            if(strcasecmp(token,"rmDisk")==0){
+                printf("***************************************\n");
+                printf("        -Comando: rmDisk-\n");
+                printf("***************************************\n");
+                rmdisk=true;
+            }
+            if(strcasecmp(token,"fdisk")==0){
+                printf("***************************************\n");
+                printf("        -Comando: fdisk-\n");
+                printf("***************************************\n");
+                fdisk=true;
             }
 
             //si  mkdisk es true reconocer los sigientes comandos
@@ -118,80 +141,158 @@ rmdiskc *  removernodo=malloc(sizeof(rmdiskc));
                 }
 
             }
-            //termina comando mkdisk
+            /*termina comando mkdisk*/
 
             //comando rmdisk
             if(strcasecmp(token,"rmDisk")==0){
                 printf("Comando: rmDisk\n");
                 rmdisk=true;
             }
+
             if(rmdisk==true){
                 if(strcasecmp(token,"-path")==0){
                     pat = true;
                 }else if(pat==true){
-strcpy(removernodo->auxpath,token);
+                    strcpy(removernodo->auxpath,token);
                     pat=false;
                 }
 
             }
-            //termina comando rmdisk
-            //printf("%s\n",token);
+            /*termina comando rmdisk*/
+
+            if(fdisk==true){
+                //atributo tamaño
+                if(strcasecmp(token,"-size")==0){
+                    size = true;
+                }else if(size==true){
+                    disk->tamanio=atoi(token);
+                    size=false;
+                }
+                //atributo nombre
+                if(strcasecmp(token,"-name")==0){
+                    name = true;
+                }else if(name==true){
+                    strcpy(disk->auxnombre,token);
+                    name=false;
+                }
+                //atributo path
+                if(strcasecmp(token,"-path")==0){
+                    pat = true;
+                }else if(pat==true){
+                    strcpy(disk->auxpath,token);
+                    pat=false;
+                }
+                //atributo unit
+                if(strcasecmp(token,"+unit")==0){
+                    unitb = true;
+                }else if(unitb==true){
+                    strcpy(disk->unidad,token);
+                    unitb=false;
+                }
+                //atributo type
+                if(strcasecmp(token,"+type")==0){
+                    type = true;
+                }else if(type==true){
+                    strcpy(disk->tipo,token);
+                    type=false;
+                }
+                //atributo fit
+                if(strcasecmp(token,"+fit")==0){
+                    fit = true;
+                }else if(fit==true){
+                    strcpy(disk->fit,token);
+                    fit=false;
+                }
+                //atributo delete
+                if(strcasecmp(token,"+delete")==0){
+                    deletee = true;
+                }else if(deletee==true){
+                    strcpy(disk->borrar,token);
+                    deletee=false;
+                }
+                //atributo add
+                if(strcasecmp(token,"+add")==0){
+                    addd = true;
+                }else if(addd==true){
+                    strcpy(disk->anadir,token);
+                    addd=false;
+                }
+
+
+            }
+            /*termina comando fdisk*/
+
+            /*parseamos los dos puntos*/
             token = strtok(NULL," ::");
+        }/*termina recorrido de token*/
 
-        }
 
+        /*ejecutamos comando rmdisk*/
         if(rmdisk==true){
             char comand[4];
-           strcpy(comand,"rm ");
+            strcpy(comand,"rm ");
             system(strcat(comand,removernodo->auxpath));
-
+            printf("***************************************\n");
+            printf("            -Disco Eliminado- \n");
+            printf("***************************************\n");
             free(removernodo);
             rmdisk=false;
+
         }
 
-        if(mkdisk==true&&nuevo->auxnombre!=""&&nuevo->auxpath!=""&&nuevo->tamanio>0){
-            if(strcasecmp(nuevo->unidad,"k")==0){
-                nuevo->tamanio=nuevo->tamanio*1024;
-        }
-    else if(strcasecmp(nuevo->unidad,"m")==0){
+        /*ejecutamos comando mkdisk*/
+        if(mkdisk==true&&nuevo->auxnombre!=""&&nuevo->auxpath!=""&&nuevo->tamanio>0&&(nuevo->unidad=="k"||nuevo->unidad=="m")){
+
             nuevo->tamanio=nuevo->tamanio*1024*1024;
-        }
-            printf("atributos listos:\n %s,%s,%d",nuevo->auxnombre,nuevo->auxpath,nuevo->tamanio);
-
-        int i=1;
-
-
-        archivo = fopen (strcat(nuevo->auxpath,nuevo->auxnombre), "wb+");
-        if(archivo==NULL){
-fclose(archivo);
-            char comao[100];
-            strcpy(comao,"mkdir -p ");
-
-            system(strcat(comao,strcat(nuevo->auxpath,nuevo->auxnombre)));
+            if(strcasecmp(nuevo->unidad,"k")==0){
+                nuevo->tamanio=nuevo->tamanio/1024;
+            }
+            int i=1;
             archivo=fopen (strcat(nuevo->auxpath,nuevo->auxnombre), "wb+");
-        }
+            if(archivo==NULL){
+                fclose(archivo);
+                char comao[100];
+                strcpy(comao,"mkdir -p ");
+                system(strcat(comao,strcat(nuevo->auxpath,nuevo->auxnombre)));
+                archivo=fopen (nuevo->auxpath, "wb+");
+            }
 
-        //mkdisk -name::archivo.bin -size::10 +unit::k -path::/home/javier/Desktop/
-        //rmDisk -path::/home/javier/Desktop/archivo.bin
-        //mkdisk -name::archivo.bin -size::25 -path::/home/javier/Desktop/pruebacarpeta/
+            char pribin[3]="\0";
+            //tamaño del disco
+            /*escritura de disco*/
+            printf("Por favor espere...\n");
+            for(i;i<=nuevo->tamanio;i++){
+                fwrite(pribin,1,sizeof(pribin),archivo);
+                fseek(archivo,i,SEEK_SET);
+            }
+            /*llenar el mbr*/
+            mbr->mbr_disk_signature=rand()%100+1;
+            /*tiempo del sistema*/
+            time_t tiempo;
+            time(&tiempo);
+            info=localtime(&tiempo);
+            strftime(mbr->mbr_fecha_creacion,17,"%x - %I:%M%p",info);
+            /*termina funcion time*/
+            mbr->mbr_tamano=nuevo->tamanio;
+            /*escribimos el mbr en el disco*/
+            fseek(archivo,0L,SEEK_SET);
+            fwrite(&mbr,sizeof(mbr),1,archivo);
+            fclose(archivo);
+            /*cerramos el archivo*/
 
-         //fprintf(archivo,"\%d",i);
-
-           //fclose(archivo);
-
-        char pribin[3]="\0";
-        //tamaño del disco
-        for(i;i<=nuevo->tamanio;i++){
-        fwrite(pribin,i,sizeof(pribin),archivo);
-        fseek(archivo,i,SEEK_SET);
-        }
-        fclose(archivo);
-
-        mkdisk=false;
-free(nuevo);
-
+            printf("***************************************\n");
+            printf("         -Disco %s Creado- \n",nuevo->auxnombre);
+            printf("***************************************\n");
+            /*Limpiamos variables y struct usados*/
+            mkdisk=false;
+            free(mbr);
+            free(nuevo);
+            nuevo=malloc(sizeof(mkdiskc));
+            mbr=malloc(sizeof(Mbrdisk));
         }else{
-            printf("error");
+            printf("***************************************\n");
+            printf("        -Error en el comando- \n");
+            printf("***************************************\n");
         }
 
 
@@ -201,3 +302,6 @@ free(nuevo);
 }
 
 
+//mkdisk -name::archivo2.dsk -size::1024 +unit::k -path::/home/javier/Desktop/pruebacarpeta/
+//rmDisk -path::/home/javier/Desktop/archivo.bin
+//mkdisk -name::archivo.dsk -size::3 -path::/home/javier/Desktop/pruebacarpeta/
